f04391ec099203fdcc2d90a9eea400d9b2e5b0bc
Abhinav Namdeo <abhaynam22@gmail.com>
2021-12-04 09:48:15 +0530

    Making todo form and about page

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
-    # return 'Hello World'
 
-@app.route('/products')
-def products():
-    return 'Produces Page'
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
