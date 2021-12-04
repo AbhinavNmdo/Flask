b2c2dc97fff3e1e1e7c15a896a2b86653064a034
Abhinav Namdeo <abhaynam22@gmail.com>
2021-12-04 10:46:38 +0530

    Making for loop in html using jinja and making db and use it as sql

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title} - {self.desc}"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    todo = Todo(title="first todo" , desc="this is discription")
+    db.session.add(todo)
+    db.session.commit()
+    alltodo = Todo.query.all()
+    print(alltodo)
+    return render_template('index.html', alltodo=alltodo)
 
 @app.route('/about')
 def about():
